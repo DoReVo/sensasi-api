7625be101938b97a5ac1f99de0ce99dd5a8166e0
DoReVo <frz565@gmail.com>
2021-04-20 23:12:43 +0800

    feat(cf-worker): add route matching and handler setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import { handleRequest } from './handler'
+import { Method } from 'tiny-request-router'
+import router from './router'
 
 addEventListener('fetch', (event) => {
-  event.respondWith(handleRequest(event.request))
+  const { pathname } = new URL(event.request.url)
+
+  // Route the request using tiny-request-router
+  const matchedRoute = router.match(event.request.method as Method, pathname)
+
+  // Call the handler if request matched with routes we specified using
+  // tiny-request-router
+  if (matchedRoute) event.respondWith(matchedRoute.handler(matchedRoute.params))
 })
